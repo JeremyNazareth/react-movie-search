75a8ca402d23a1f09e89f3b059bd1f77dd8db429
melissa riquelme <jer.reyesr@outlook.com>
2025-02-02 17:14:46 -0300

    script de descarga de peliculas corregido y ya descargada la informacion

--- src/assets/data/downloadMovies.js
+++ src/assets/data/downloadMovies.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const fetch = require('node-fetch');
+import fs from 'fs';
+import fetch from 'node-fetch';
 
-const API_KEY = '';
+const API_KEY = '93561c86d4e411400015148457559807';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
