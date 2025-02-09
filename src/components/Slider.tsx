5a175393ad6de0cf6166ad4f04494a9ada730a4f
JeremyNazareth <jer.reyesr@outlook.com>
2025-02-09 18:07:10 -0300

    base de slider terminado

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -3,7 +3,7 @@ import styles from "./Slider.module.css"
 import topRated from "../assets/data/topRated.json"
 
 const Slider = ({movies}) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(1);
 
     const nextSlide = () =>{
         setCurrentIndex((prevIndex)=> (prevIndex + 1) % movies.length)
@@ -17,15 +17,23 @@ const Slider = ({movies}) => {
         <div className={styles.Slider}>
             <div className={styles.SliderBorder}>
                 <div className={styles.SliderContent} style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-                    <img className={styles.SliderImg} src={`https://image.tmdb.org/t/p/w500${movies[currentIndex].poster_path}`} alt="" />
-                    <p>{console.log(currentIndex)}{movies[currentIndex].title}</p>
+                    {movies.map((movie) =>(
+                        <div className={styles.SliderItem} >
+                            <img className={styles.SliderImg} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+                            {movie.title}
+                        </div>
+                    ))}
                 </div>
             </div>
-            
+            <button onClick={prevSlide}>Right</button>
+            <button onClick={nextSlide}>Left</button>
         </div>
     )
     
 }
 
-
+{/*
+    <img className={styles.SliderImg} src={`https://image.tmdb.org/t/p/w500${movies[currentIndex].poster_path}`} alt="" />
+    <p>{console.log(currentIndex)}{movies[currentIndex].title}</p>     
+*/}
 export default Slider;
