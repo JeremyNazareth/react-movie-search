2cfc1d1f9c3b3df5ea6458bb6ac37ee76fde5a10
JeremyNazareth <jer.reyesr@outlook.com>
2025-02-10 18:58:41 -0300

    avance de diseño de slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,32 +1,51 @@
 import { useEffect, useState } from "react";
-import styles from "./Slider.module.css"
-import topRated from "../assets/data/topRated.json"
+import styles from "./Slider.module.css";
+import {MoveRight, MoveLeft} from 'lucide-react';
 
 const Slider = ({movies}) => {
-    const [currentIndex, setCurrentIndex] = useState(1);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const nextSlide = () =>{
         setCurrentIndex((prevIndex)=> (prevIndex + 1) % movies.length)
     };
 
     const prevSlide = () =>{
-        setCurrentIndex((prevIndex)=> (prevIndex - 1) % movies.length)
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + movies.length) % movies.length);
     }
 
     return(
         <div className={styles.Slider}>
+            <button className={styles.MoveButton} onClick={prevSlide}><MoveLeft size={40} strokeWidth={3}></MoveLeft></button>
             <div className={styles.SliderBorder}>
                 <div className={styles.SliderContent} style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
                     {movies.map((movie) =>(
                         <div className={styles.SliderItem} >
-                            <img className={styles.SliderImg} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
-                            {movie.title}
+                            <img className={styles.ItemImg} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+                            <div className={styles.ItemText}>
+                                <p className={styles.ItemTitle}>{movie.title}</p>
+                                <hr className={styles.ItemHr} />
+                                <p className={styles.ItemOverview}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam molestiae similique unde deserunt vel ut ipsum voluptatum id. Esse ex commodi officia ipsa, odit nulla facilis veniam atque perferendis corrupti.</p>
+                                <hr className={styles.ItemHr} />
+                                <div className={styles.ItemDescription}>
+                                    <p>Descripcion1: descripcion 1</p>
+                                    <p>Descripcion2: descripcion 2</p>
+                                    <p>Descripcion3: descripcion 3</p>
+                                </div>
+                                <hr className={styles.ItemHr} />
+                                <p>Generos</p>
+                                <div className={styles.ItemGenres}>
+                                    <p>Genero1</p>
+                                    <p>Genero2</p>
+                                    <p>Genero3</p>
+                                    <p>Genero4</p>
+                                    
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
             </div>
-            <button onClick={prevSlide}>Right</button>
-            <button onClick={nextSlide}>Left</button>
+            <button className={styles.MoveButton} onClick={nextSlide}><MoveRight size={40} strokeWidth={3}></MoveRight></button>
         </div>
     )
     
