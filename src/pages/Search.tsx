4a8df8c0d0aae1c6bffdbbdf935c1fe77fba2640
JeremyNazareth <jer.reyesr@outlook.com>
2025-03-03 19:07:34 -0300

    estructura de los filtros por genero listo

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -6,6 +6,7 @@ const Search = () => {
 
     const {search} = useParams();
 
+    const genresList = ['Drama','Comedia','Accion','Aventura']
     const filteredMovies = movies.filter((movie) =>
         movie.title.toLowerCase().includes(search ? search  : "Desconocido")
     )
@@ -16,8 +17,15 @@ const Search = () => {
                     Mostrando resultados para "{search}"
                 </p>
                 <div className={styles.content}>
-                    <div className={styles.genresFilter}>
-                        
+                    <div className={`card  ${styles.genresFilter}`}>
+                        <div className={`card-header`}>
+                            <p className={styles.filterTitle}>Filtros</p>
+                        </div>
+                        <ul className="list-group">
+                            {genresList.map((genre, index) =>
+                                <li className={`list-group-item ${styles.liFilter}`}><input type="checkbox" className="form-check-input" id="flexCheckDefault" /><label htmlFor="" className={styles.checkboxLabel}>{genre}</label></li>
+                            )}                       
+                        </ul>
                     </div>
                     <div className={styles.searchedMovies}>
                         {filteredMovies.map((movie) =>(
