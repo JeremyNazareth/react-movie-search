4b21afa0e53c62d4e9feaeb3767be26be43c1b7c
JeremyNazareth <jer.reyesr@outlook.com>
2025-03-10 18:10:09 -0300

    detalles de pelicula recuperadas en movieDetails

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -4,11 +4,13 @@ import movieDetails from '../assets/data/movieDetails.json'
 const MovieDetails = () => {
 
     const {id} = useParams();
+    const movie = movieDetails.find((movieData) => movieData.id.toString() === id)
+
     
     return(
         <div>
             <h1>Movie Details</h1>
-            <h2>{id? id : "Pelicula no encontrada"}</h2>
+            <h2>{movie?.original_title}</h2>
         </div>
         
     )
