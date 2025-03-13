1912f9fe00d85ba6b40676582e7ee2461d108708
JeremyNazareth <jer.reyesr@outlook.com>
2025-03-13 18:18:02 -0300

    sistema de descarga de datos actualizado con variable segura

--- src/assets/data/downloadMovies.js
+++ src/assets/data/downloadMovies.js
@@ -1,8 +1,13 @@
 import fs from 'fs';
 import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 
-const API_KEY = '';
+dotenv.config({path: '../../../.env'});
+
+const API_KEY = process.env.TMDB_API_KEY
 const BASE_URL = 'https://api.themoviedb.org/3';
+
+console.log(API_KEY)
 
 fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
     .then(response => response.json())
